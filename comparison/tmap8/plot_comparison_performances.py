d73c3027b779c8914435889cfffba578b25de8b8
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-09-13 18:09:10 -0400

    better plotting

--- comparison/tmap8/plot_comparison_performances.py
+++ comparison/tmap8/plot_comparison_performances.py
@@ -16,13 +16,15 @@ tmap8_times = [case['tmap'] for case in data.values()]
 bartmap = plt.barh(ypos + width, tmap8_times, width, label='TMAP8')
 barfestim = plt.barh(ypos, festim_times, width, label='FESTIM')
 
-plt.bar_label(barfestim, padding=8, fmt='%.1f s', fontsize=14)
-plt.bar_label(bartmap, padding=8, fmt='%.1f s', fontsize=14)
+plt.bar_label(barfestim, padding=8, fmt='%.1f s', fontsize=10, color=barfestim.patches[0].get_facecolor())
+plt.bar_label(bartmap, padding=8, fmt='%.1f s', fontsize=10, color=bartmap.patches[0].get_facecolor())
 
 plt.gca().set_yticks(ypos + width/2, labels=labels)
-plt.gca().set_xticks([])
+plt.gca().set_xticks([20, 40, 60])
 
-plt.legend()
+plt.legend(bbox_to_anchor=(0.7, 1.1), frameon=False, ncol=2)
 plt.gca().spines[['left', 'right', 'top', 'bottom']].set_visible(False)
+plt.grid(True, which='major', axis="x", alpha=0.3)
+plt.xlabel("Execution time (s)")
 plt.tight_layout()
 plt.show()
