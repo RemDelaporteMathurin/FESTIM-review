1fdbd21ffccd3c7bb8e52585cc39c14258a950c0
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-09-13 07:58:21 -0400

    fixed val-2b.i TMAP8 and modified plotting script

--- comparison/tmap8/val_2b/plot.py
+++ comparison/tmap8/val_2b/plot.py
@@ -10,36 +10,21 @@ temp2 = data["Average_T_volume_2"]
 
 desorption_flux = -2*data["Flux_surface_1_solute"]
 
-data_tmap8 = np.genfromtxt("results/tmap8_results.csv", names=True, delimiter=",")
+data_tmap8 = np.genfromtxt("val-2b_out.csv", names=True, delimiter=",")
 
 t_tmap8 = data_tmap8["time"]
 idx_tmap8 = np.where(t_tmap8 > 182400)
-temp1_tmap8 = data_tmap8["Temp"]
+temp1_tmap8 = data_tmap8["Temp"][idx_tmap8]
 
 scaling_factor = 1e20
 desorption_flux_tmap8 = 2*data_tmap8["avg_flux_left"] * scaling_factor
-
-data_tmap8_short_timestep = np.genfromtxt("results/tmap8_results.csv", names=True, delimiter=",")
-
-t_tmap8_short_timestep = data_tmap8_short_timestep["time"]
-idx_tmap8_short_timestep = np.where(t_tmap8 > 182400)
-temp1_tmap8_short_timestep = data_tmap8_short_timestep["Temp"]
-
-scaling_factor = 1e20
-desorption_flux_tmap8_short_timestep = 2*data_tmap8_short_timestep["avg_flux_left"] * scaling_factor
-
-data_loading = np.genfromtxt("results/derived_quantities_loading.csv", names=True, delimiter=",")
-
-t_loading = data_loading["ts"]
-temp1_loading = data_loading["Average_T_volume_1"]
-temp2_loading = data_loading["Average_T_volume_2"]
+desorption_flux_tmap8 = desorption_flux_tmap8[idx_tmap8]
 
 data_exp = np.genfromtxt("results/experimental_data.csv", names=True, delimiter=",")
 temp_exp = data_exp["temp"]
 flux_exp = data_exp["flux"] * 1e15  # tmap8 people scaled it
 
 plt.plot(temp1, desorption_flux)
-# plt.plot(temp1_tmap8[idx_tmap8], desorption_flux_tmap8[idx_tmap8])
-plt.plot(temp1_tmap8_short_timestep[idx_tmap8_short_timestep], desorption_flux_tmap8_short_timestep[idx_tmap8])
+plt.plot(temp1_tmap8, desorption_flux_tmap8)
 plt.scatter(temp_exp, flux_exp)
 plt.show()
