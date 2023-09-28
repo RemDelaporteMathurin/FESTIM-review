55e08e6f1e3f1bd98ca1fe1035553d8260bf99f8
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-09-28 11:53:00 -0400

    converts also to XDMF

--- comparison/achlys/convert_mesh_to_msh.py
+++ comparison/achlys/convert_mesh_to_msh.py
@@ -12,6 +12,9 @@ cell_data = {"gmsh:geometrical": [
 correspondance_dict = mesh.cell_tags
 print(correspondance_dict)
 
+
+# WRITE TO GMSH FORMAT
+
 meshio.write_points_cells(
     "mesh.msh",
     mesh.points,
@@ -19,3 +22,25 @@ meshio.write_points_cells(
     cell_data=cell_data,
     file_format='gmsh22'
 )
+
+# WRITE TO XDMF FORMAT
+
+cell_data_types = mesh.cell_data_dict["cell_tags"].keys()
+
+cell_type = "tetra"
+facet_type = "triangle"
+for mesh_block in mesh.cells:
+    if mesh_block.type == cell_type:
+        meshio.write_points_cells(
+            "mesh_cells.xdmf",
+            mesh.points,
+            [mesh_block],
+            cell_data={"f": [-1 * mesh.cell_data_dict["cell_tags"][cell_type]]},
+        )
+    elif mesh_block.type == facet_type:
+        meshio.write_points_cells(
+            "mesh_facets.xdmf",
+            mesh.points,
+            [mesh_block],
+            cell_data={"f": [-1 * mesh.cell_data_dict["cell_tags"][facet_type]]},
+        )
