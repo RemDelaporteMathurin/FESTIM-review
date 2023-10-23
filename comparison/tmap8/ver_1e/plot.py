2f9da44836e97f0bf350cd991c4330ff228fba31
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-10-23 11:18:38 -0400

    added transparency for better vis

--- comparison/tmap8/ver_1e/plot.py
+++ comparison/tmap8/ver_1e/plot.py
@@ -2,15 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data = np.genfromtxt("results.csv", names=True, delimiter=",")
-data_tmap8 = np.genfromtxt('ver-1e_out.csv', names=True, delimiter=',')
+data_tmap8 = np.genfromtxt("ver-1e_out.csv", names=True, delimiter=",")
 
 for i, x_pos in enumerate([20e-6, 48.8e-6]):
+    mobile_conc_festim = data[
+        f"solute_value_{x_pos:.2e}".replace("+", "").replace(".", "").replace("-", "")
+    ]
+    plt.plot(
+        data["ts"],
+        mobile_conc_festim,
+        color="tab:blue",
+        label="FESTIM",
+        zorder=1,
+        linewidth=3,
+    )
+    plt.plot(
+        data_tmap8["time"],
+        data_tmap8[f"conc_point{i+1}"],
+        color="tab:orange",
+        linestyle="solid",
+        label="TMAP8",
+        zorder=2,
+        alpha=0.85,
+    )
 
-    mobile_conc_festim = data[f"solute_value_{x_pos:.2e}".replace('+', '').replace('.', '').replace('-', '')]
-    plt.plot(data["ts"], mobile_conc_festim, color='tab:blue', label="FESTIM", zorder=1, linewidth=3)
-    plt.plot(data_tmap8["time"], data_tmap8[f"conc_point{i+1}"], color='tab:orange', linestyle='solid', label="TMAP8", zorder=2)
-
-    plt.annotate(f'$x={x_pos*1e6} $ μm', (data_tmap8["time"][-1] + 1, mobile_conc_festim[-1]), va='center')
+    plt.annotate(
+        f"$x={x_pos*1e6} $ μm",
+        (data_tmap8["time"][-1] + 1, mobile_conc_festim[-1]),
+        va="center",
+    )
 
 handles, labels = plt.gca().get_legend_handles_labels()
 by_label = dict(zip(labels, handles))
@@ -18,11 +38,11 @@ plt.legend(by_label.values(), by_label.keys())
 
 # plt.xlim(left=0)
 plt.ylim(bottom=0)
-plt.gca().spines[['right', 'top']].set_visible(False)
-plt.grid(True, which='major', alpha=0.3)
+plt.gca().spines[["right", "top"]].set_visible(False)
+plt.grid(True, which="major", alpha=0.3)
 
-plt.xlabel('Time (s)')
-plt.ylabel('Mobile concentration')
+plt.xlabel("Time (s)")
+plt.ylabel("Mobile concentration")
 plt.tight_layout()
 
 for ext in ["png", "svg", "pdf"]:
