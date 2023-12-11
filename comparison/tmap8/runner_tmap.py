6706d7303cdf578495974410755200e489e83e82
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-12-11 16:59:33 +0100

    renamed tmap file + added to runner

--- comparison/tmap8/runner_tmap.py
+++ comparison/tmap8/runner_tmap.py
@@ -2,38 +2,41 @@ import subprocess
 import timeit
 import json
 
-path_to_tmap_executable = './tmap8-opt'
+path_to_tmap_executable = "./tmap8-opt"
+
 
 def run_external_script(script_path):
     try:
-        subprocess.run([path_to_tmap_executable, '-i', script_path])
+        subprocess.run([path_to_tmap_executable, "-i", script_path])
     except subprocess.CalledProcessError as e:
         print(f"Error running the executable: {e}")
+
 
 nb_exec = 5
 
 case_to_path = {
-    "ver-1e": 'ver_1e/ver-1e.i',
-    "val-2b": 'val_2b/val-2b.i',
-    "ver-1d-diffusion": 'ver_1d/ver-1d-diffusion.i',
-    "ver-1c": 'ver_1c/ver-1c.i'
+    "ver-1a": "ver_1a/ver-1a.i",
+    "ver-1e": "ver_1e/ver-1e.i",
+    "val-2b": "val_2b/val-2b.i",
+    "ver-1d-diffusion": "ver_1d/ver-1d-diffusion.i",
+    "ver-1c": "ver_1c/ver-1c.i",
 }
 
 case_to_time = {case: None for case in case_to_path}
 
 for case, path in case_to_path.items():
     time_festim = timeit.timeit(lambda: run_external_script(path), number=nb_exec)
-    case_to_time[case] = time_festim/nb_exec
+    case_to_time[case] = time_festim / nb_exec
 
 # write results to JSON
-with open('comparison_results.json') as f:
+with open("comparison_results.json") as f:
     data = json.load(f)
 
 for case, time in case_to_time.items():
     if case not in data:
-        data[case] = {'festim': 0, 'tmap': time}
+        data[case] = {"festim": 0, "tmap": time}
     else:
-        data[case]['tmap'] = time
+        data[case]["tmap"] = time
 
-with open('comparison_results.json', "w") as f:
+with open("comparison_results.json", "w") as f:
     json.dump(data, f, indent=4)
