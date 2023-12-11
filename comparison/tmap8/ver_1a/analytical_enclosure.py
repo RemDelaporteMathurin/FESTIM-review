d34636cb0b9589f92adce894332a2583d69c3ae6
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-12-11 16:53:58 +0100

    added ver1a festim

---
+++ comparison/tmap8/ver_1a/analytical_enclosure.py
@@ -0,0 +1,178 @@
+import numpy as np
+
+from scipy.integrate import cumtrapz
+
+import matplotlib.pyplot as plt
+
+# https://stackoverflow.com/questions/28766692/how-to-find-the-intersection-of-two-graphs/28766902#28766902
+
+k = 1.38065e-23  # J/mol Boltzmann constant
+
+
+def get_roots(L, l, alpha_max, step=0.0001):
+    """Gets the roots of alpha = L / tan(alpha * l)
+
+    Args:
+        L (float): parameter L
+        l (float): parameter l
+        alpha_max (float): the maximum alpha to consider
+        step (float, optional): the step discretising alphas.
+            The smaller the step, the more accurate the roots.
+            Defaults to 0.0001.
+
+    Returns:
+        np.array: array of roots
+    """
+    alphas = np.arange(0, alpha_max, step=step)[1:]
+
+    f = alphas
+
+    g = L / np.tan(alphas * l)
+
+    # plt.plot(alphas, f, "-")
+    # plt.plot(alphas, g, "-")
+
+    idx = np.argwhere(np.diff(np.sign(f - g))).flatten()
+
+    # remove one every other idx
+    idx = idx[::2]
+    # plt.plot(alphas[idx], f[idx], "ro")
+    # plt.show()
+    roots = alphas[idx]
+    return roots
+
+
+def get_roots_bis(L, N, step=0.0001):
+    alphas = np.arange(0, N, step=step)[1:]
+
+    f = alphas
+
+    g = L / np.tan(alphas)
+
+    plt.plot(alphas, f, "-")
+    plt.plot(alphas, g, "-")
+
+    idx = np.argwhere(np.diff(np.sign(f - g))).flatten()
+
+    # remove one every other idx
+    idx = idx[::2]
+    plt.plot(alphas[idx], f[idx], "ro")
+    plt.show()
+    roots = alphas[idx]
+    return roots
+
+
+def analytical_expression_fractional_release(t, P_0, D, S, V, T, A, l):
+    """
+    FR = 1 - P(t) / P_0
+    where P(t) is the pressure at time t and P_0 is the initial pressure
+
+    Args:
+        t (float, ndarray): time (s)
+        P_0 (float): initial presure (Pa)
+        D (float): diffusivity (m2/s)
+        S (float): solubility (H/m3/Pa)
+        V (float): enclosure volume (m3)
+        T (float): temperature (K)
+        A (float): enclosure surface area (m2)
+        l (float): slab length (m)
+    """
+    L = S * T * A * k / V
+
+    roots = get_roots(L=L, l=l, alpha_max=1e6, step=1)
+    print(len(roots))
+    roots = roots[:, np.newaxis]
+    summation = np.exp(-(roots**2) * D * t) / (l * (roots**2 + L**2) + L)
+    last_term = summation[-1]
+    summation = np.sum(summation, axis=0)
+
+    print(last_term / summation)
+    pressure = 2 * P_0 * L * summation
+    fractional_release = 1 - pressure / P_0
+    return fractional_release
+
+
+# FIXME this doesn't work
+# taken from https://mooseframework.inl.gov/TMAP8/verification/ver-1a.html#longhurst1992verification
+def analytical_expression_cumulative_release(t, D, S, V, T, A, l):
+    phi = 1 / (k * T * S)
+    L = l * A / (V * phi)
+    roots = get_roots_bis(L=L, alpha_max=2000, step=1e-3)
+    roots = roots[:, np.newaxis]
+    sec = 1 / np.cos(roots)
+    summation = (2 * L * sec - np.exp(-(roots**2) * D * t / l**2)) / (
+        L * (L + 1) + roots**2
+    )
+    last_term = summation[-1]
+    summation = np.sum(summation, axis=0)
+    print(summation[0])
+    print(last_term / summation)
+    cumulative_release = 1 - summation
+    return cumulative_release
+
+
+def analytical_expression_flux(t, P_0, D, S, V, T, A, l):
+    """
+    value of the flux at the external surface (not in contact with pressure)
+    J = -D * dc/dx
+
+    Args:
+        t (float, ndarray): time (s)
+        P_0 (float): initial presure (Pa)
+        D (float): diffusivity (m2/s)
+        S (float): solubility (H/m3/Pa)
+        V (float): enclosure volume (m3)
+        T (float): temperature (K)
+        A (float): enclosure surface area (m2)
+        l (float): slab length (m)
+    """
+    L = S * T * A * k / V
+
+    roots = get_roots(L=L, l=l, alpha_max=1e7, step=1)
+    roots = roots[:, np.newaxis]
+
+    summation = (np.exp(-(roots**2) * D * t) * roots) / (
+        (l * (roots**2 + L**2) + L) * np.sin(roots * l)
+    )
+    last_term = summation[-1]
+    summation = np.sum(summation, axis=0)
+
+    print(last_term / summation)
+    flux = 2 * S * P_0 * L * D * summation
+    return flux
+
+
+def cumulative_flux(t, P_0, D, S, V, T, A, l):
+    flux = analytical_expression_flux(t, P_0, D, S, V, T, A, l)
+
+    cumulative_flux = cumtrapz(flux, t, initial=0)
+    initial_quantity = P_0 * V / k / T
+
+    return cumulative_flux * A / initial_quantity
+
+
+if __name__ == "__main__":
+    T = 2373
+    times = np.linspace(0, 45, 1000)
+    cum_flux = cumulative_flux(
+        t=times,
+        P_0=1e6,
+        D=2.6237e-11,
+        S=7.244e22 / T,
+        V=5.20e-11,
+        T=T,
+        A=2.16e-6,
+        l=3.3e-5,
+    )
+    # flux = analytical_expression_flux(
+    #     t=times,
+    #     P_0=1e6,
+    #     D=2.6237e-11,
+    #     S=7.244e22 / T,
+    #     V=5.20e-11,
+    #     T=T,
+    #     A=2.16e-6,
+    #     l=3.3e-5,
+    # )
+    plt.plot(times, cum_flux)
+    plt.show()
---
+++ comparison/tmap8/ver_1a/enclosure_explicit.py
@@ -0,0 +1,168 @@
+import festim as F
+import numpy as np
+import fenics as f
+from analytical_enclosure import (
+    analytical_expression_fractional_release,
+    cumulative_flux,
+)
+
+from scipy.integrate import cumtrapz
+
+encl_vol = 5.20e-11  # m3  same
+encl_surf = 2.16e-6  # m2  same
+l = 3.3e-5  # m same
+R = 8.314  # same
+avogadro = 6.022e23  # mol-1  same
+temperature = 2373  # K  same
+initial_pressure = 1e6  # Pa  same
+solubility = 7.244e22 / temperature  # H/m3/Pa  # same
+diffusivity = 2.6237e-11  # m2/s  almost same
+
+
+def henrys_law(T, S_0, E_S, pressure):
+    S = S_0 * f.exp(-E_S / F.k_B / T)
+    return S * pressure
+
+
+class PressureExport(F.DerivedQuantity):
+    def __init__(self, **kwargs):
+        super().__init__(field="solute", **kwargs)
+        self.title = "enclosure_pressure"
+        self.data = []
+
+    def compute(self):
+        return float(left_bc.pressure)
+
+
+class CustomHenrysBC(F.HenrysBC):
+    def create_expression(self, T):
+        value_BC = F.BoundaryConditionExpression(
+            T,
+            henrys_law,
+            S_0=self.H_0,
+            E_S=self.E_H,
+            pressure=self.pressure,
+        )
+        self.expression = value_BC
+        self.sub_expressions = [self.pressure]
+
+
+class CustomSimulation(F.Simulation):
+    def iterate(self):
+        super().iterate()
+        # Update pressure based on flux
+        left_flux_val = left_flux.compute()
+        old_pressure = float(left_bc.pressure)
+        new_pressure = (
+            old_pressure
+            - (left_flux_val * encl_surf / encl_vol * R * self.T.T(0) / avogadro)
+            * self.dt.value
+        )
+        left_bc.pressure.assign(new_pressure)
+
+
+my_model = CustomSimulation()
+
+vertices = np.linspace(0, l, 100)
+
+my_model.mesh = F.MeshFromVertices(vertices)
+
+my_model.materials = F.Material(
+    id=1,
+    D_0=diffusivity,
+    E_D=0,
+)
+
+left_bc = CustomHenrysBC(
+    surfaces=1, H_0=solubility, E_H=0, pressure=f.Constant(initial_pressure)
+)
+
+my_model.boundary_conditions = [
+    left_bc,
+    F.DirichletBC(surfaces=2, value=0, field="solute"),
+]
+
+my_model.T = F.Temperature(temperature)
+
+my_model.settings = F.Settings(
+    absolute_tolerance=1e8,
+    relative_tolerance=1e-10,
+    final_time=45,
+)
+
+left_flux = F.HydrogenFlux(surface=1)
+right_flux = F.HydrogenFlux(surface=2)
+pressure_export = PressureExport()
+derived_quantities = F.DerivedQuantities([left_flux, right_flux, pressure_export])
+
+my_model.exports = [
+    # F.XDMFExport("solute", filename="enclosure/mobile.xdmf", checkpoint=False),
+    derived_quantities,
+]
+
+my_model.dt = F.Stepsize(initial_value=0.1)
+
+
+my_model.initialise()
+my_model.run()
+print(f"final pressure is {float(left_bc.pressure)}")
+
+
+# ------------ post processing ----------------
+
+t = derived_quantities.t
+pressures = np.array(pressure_export.data)
+fractional_release = 1 - pressures / initial_pressure
+right_flux = np.abs(right_flux.data)
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.plot(t, fractional_release, linestyle="--", label="FESTIM")
+
+times = np.linspace(0, my_model.settings.final_time, 1000)
+analytical = analytical_expression_fractional_release(
+    t=times,
+    P_0=initial_pressure,
+    D=my_model.materials.materials[0].D_0,
+    S=left_bc.H_0,
+    V=encl_vol,
+    T=temperature,
+    A=encl_surf,
+    l=l,
+)
+plt.plot(times, analytical, label="analytical")
+plt.legend()
+plt.xlabel("Time (s)")
+plt.ylabel("Fractional release")
+plt.grid(alpha=0.3)
+
+plt.figure()
+plt.plot(t, right_flux)
+
+plt.figure()
+
+initial_quantity = initial_pressure * encl_vol / R / temperature * avogadro
+cumulative_released = cumtrapz(right_flux, t, initial=0) * encl_surf
+
+plt.plot(t, cumulative_released / initial_quantity, linestyle="--", label="FESTIM")
+
+analytical = cumulative_flux(
+    t=times,
+    P_0=initial_pressure,
+    D=my_model.materials.materials[0].D_0,
+    S=left_bc.H_0,
+    V=encl_vol,
+    T=temperature,
+    A=encl_surf,
+    l=l,
+)
+plt.plot(times, analytical, label="analytical")
+
+plt.legend()
+plt.xlabel("Time (s)")
+plt.ylabel("Cumulative release")
+plt.grid(alpha=0.3)
+
+
+plt.show()
